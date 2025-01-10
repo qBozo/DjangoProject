1d310b629fdcd15c939d3222cc3024cc3554b7ed
qBozo <beaumjohnston@gmail.com>
2025-01-10 12:41:54 +1100

    fixes to some errors during tutorial part 3

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
@@ -9,3 +15,4 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+    
