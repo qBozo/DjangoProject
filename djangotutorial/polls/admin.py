30e96a099dec4e7210f3dcc0ff6ea0941efe472e
qBozo <beaumjohnston@gmail.com>
2025-01-14 13:51:07 +1100

    continuation of part 7 django tutorial - added admin directory to admin and copied base_site.html from django source code

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -13,7 +13,10 @@ class QuestionAdmin(admin.ModelAdmin):
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
     inlines = [ChoiceInline]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
 
 
 admin.site.register(Question, QuestionAdmin)
